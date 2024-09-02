0f6b80aa4adfc5ca1d231013addb931108506cbc
Francini <110108716+francielyy@users.noreply.github.com>
2024-09-02 08:45:59 -0300

    02/09

---
+++ js/entrevistas.js
@@ -0,0 +1,116 @@
+let entrevista1 = {
+    entrevistador: "fran",
+    entrevistado: "izaLinda",
+    questoes:[
+    {p1: "Quais são os cursos academicos que você faria? Por quê?",
+     r1: "Medicina, pois meu sonho é ser pediatra e ter minha própria clínica, além de querer querer que as crianças percam o medo de ir ao médico porque serei uma médica muito legal legal"},
+    
+     {p2: "Qual seu carro dos sonhos?",
+    r2:"Audi RS5 branco com teto solar"},
+    
+    {p3: "Você pretende continuar morando na cidade que reside atualmente?",
+    r3:"Sim, pretendo"},
+
+    {p4: "Você faria uma expedição para outro planeta?",
+    r4:"Sim, Marte, Saturno e Netuno"},
+
+    {p5: "Faria algum intercâmbio?",
+    r5:"Sim, na faculdade"},
+
+    {p6: "Tem o sonho de casar? Pretende ter filhos? Quantos?",
+    r6:"SIM, SIM, ter 2 lindos filhos a cara da mãe"},
+
+    {p7: "Qual seu maior sonho?",
+    r7:"Cursar medicina na Unicesumar com bolsa 100%"},
+
+    {p8: "Acha que com suas escolhas você seria feliz?",
+    r8:"Sim!"}
+    ]
+}
+let entrevista2 = {
+    entrevistador: "izaLinda",
+    entrevistado: "fran",
+    questoes:[
+    {p1: "Quais são os cursos academicos que você faria? Por quê?",
+     r1: "Medicina, pois gosto de entender sobre o corpo humano e descobrir tratamentos alternativos para doenças de difícil de tratar"},
+    
+     {p2: "Qual seu carro dos sonhos?",
+    r2:"Não tenho"},
+    
+    {p3: "Você pretende continuar morando na cidade que reside atualmente?",
+    r3:"Não"},
+
+    {p4: "Você faria uma expedição para outro planeta?",
+    r4:"Sim, Urano"},
+
+    {p5: "Faria algum intercâmbio?",
+    r5:"Sim, para Itália e Etados Unidos"},
+
+    {p6: "Tem o sonho de casar? Pretende ter filhos? Quantos?",
+    r6:"Sim, Sim, 2 filhos, mas somente quando estiver estabilizada profissionlmente"},
+
+    {p7: "Qual seu maior sonho?",
+    r7:"Me tornar uma médica pesquisadora conhecida"},
+
+    {p8: "Acha que com suas escolhas você seria feliz?",
+    r8:"Com certeza"}
+    ]
+}
+let entrevista3 = {
+    entrevistador: "fran",
+    entrevistado: "nanay",
+    questoes:[
+    {p1: "Quais são os cursos academicos que você faria? Por quê?",
+     r1:"Ciências contábeis, direito, administração, medicina veterinária e biologia, os três primeiros para administrar empresas e os outros dois porque gosto de animais e quero ajuda-los"},
+    
+     {p2: "Qual seu carro dos sonhos?",
+    r2:"Mitsubish lancer GTI modificada roxa"},
+    
+    {p3: "Você pretende continuar morando na cidade que reside atualmente?",
+    r3:"Sim"},
+
+    {p4: "Você faria uma expedição para outro planeta?",
+    r4:"Sim, para Jupter ou outra galáxia"},
+
+    {p5: "Faria algum intercâmbio?",
+    r5:"Sim, para todos os países possíveis"},
+
+    {p6: "Tem o sonho de casar? Pretende ter filhos? Quantos?",
+    r6:"Sim, Sim, ter um menino e uma menina"},
+
+    {p7: "Qual seu maior sonho?",
+    r7:"Ser rica"},
+
+    {p8: "Acha que com suas escolhas você seria feliz?",
+    r8:"Sim"}
+    ]
+}
+let entrevista4 = {
+    entrevistador: "fran",
+    entrevistado: "calvinha",
+    questoes:[
+    {p1: "Quais são os cursos academicos que você faria? Por quê?",
+     r1: "Comércio Exterior, terapia ocupacional e moda, pois me identifico com eles"},
+    
+     {p2: "Qual seu carro dos sonhos?",
+    r2:"Não tenho"},
+    
+    {p3: "Você pretende continuar morando na cidade que reside atualmente?",
+    r3:"Sim, mas se tivesse oportunidade de morar no exterior eu iria"},
+
+    {p4: "Você faria uma expedição para outro planeta?",
+    r4:"Jupter e netuno"},
+
+    {p5: "Faria algum intercâmbio?",
+    r5:"Com certeza, para todos os lugares que pudesse"},
+
+    {p6: "Tem o sonho de casar? Pretende ter filhos? Quantos?",
+    r6:"Sim, sim, teria 2 filhos e um golden"},
+
+    {p7: "Qual seu maior sonho?",
+    r7:"Me realizar profissonalmente e ter minha família"},
+
+    {p8: "Acha que com suas escolhas você seria feliz?",
+    r8:"Sim"}
+    ]
+}
